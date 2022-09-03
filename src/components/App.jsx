a2364ac111d79173ea878d6d9310658c848130c5
Kostiantyn <cashalot80@ukr.net>
2022-09-03 22:32:58 +0100

    finish task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,96 @@
-export const App = () => {
+import { useEffect, useState } from 'react';
+import Main from './Main/Main';
+import { nanoid } from 'nanoid';
+import Phonebook from './Phonebook/Phonebook';
+import Contacts from './Phonebook/Contacts/Contacts';
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const localContacts = window.localStorage.getItem('contacts');
+    const parcedContacts = JSON.parse(localContacts);
+    return parcedContacts || [];
+  });
+
+  const [filter, setFilter] = useState('');
+  // state = {
+  //   contacts: [],
+  //   filter: '',
+  // };
+
+  const handleChange = event => {
+    setFilter(event.currentTarget.value);
+
+    // const { name, value } = event.currentTarget;
+    // this.setState({ [name]: value });
+  };
+
+  const formSubmitHandle = data => {
+    const id = nanoid();
+    if (contacts.filter(contacts => contacts.name === data.name).length > 0) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+    setContacts(prevState => {
+      return [
+        ...prevState,
+        {
+          name: data.name,
+          number: data.number,
+          id: id,
+        },
+      ];
+    });
+  };
+
+  const onClickDelete = id => {
+    setContacts(contacts.filter(contacts => contacts.id !== id));
+  };
+
+  // componentDidMount() {
+  //   // const contacts = localStorage.getItem('contacts');
+  //   // const parsedContacts = JSON.parse(contacts);
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts });
+  //   }
+  // }
+
+  // componentDidUpdate(prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     // console.log(this.state.contacts);
+  //     localStorage.setItem('contacs', JSON.stringify(this.state.contacs));
+  //   }
+  // }
+
+  const normalizeFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contacts =>
+    contacts.name.toLowerCase().includes(normalizeFilter)
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Main title="Phonebook">
+      <Phonebook
+        onChange={handleChange}
+        onSubmit={formSubmitHandle}
+        contactsList={visibleContacts}
+        notEmptyList={setContacts.length}
+        valueFilter={setFilter}
+      />
+      {this.state.contacts.length > 0 ? (
+        <Contacts
+          name="Contacts"
+          contactsList={visibleContacts}
+          onChange={handleChange}
+          value={setFilter}
+          onClickDelete={onClickDelete}
+        />
+      ) : (
+        <p>Phonebook empty</p>
+      )}
+    </Main>
   );
 };
+export default App;
